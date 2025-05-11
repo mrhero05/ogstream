958a9c663f6d5de68d8cca27d8e3cec7edf693af
mrhero05 <ogiesanchez.hs@gmail.com>
2025-05-11 23:53:26 +0800

    Added chadcn ui

---
+++ resources/js/Components/Sections/Banner.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/Components/ui/carousel'
+</script>
+
+<template>
+  <Carousel class="relative">
+    <CarouselContent>
+      <CarouselItem>1</CarouselItem>
+      <CarouselItem>2</CarouselItem>
+      <CarouselItem>3</CarouselItem>
+    </CarouselContent>
+    <div class="absolute translate-1/2">
+        <CarouselPrevious class="" />
+        <CarouselNext />
+    </div>
+  </Carousel>
+</template>
---
+++ resources/js/Pages/Homepage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import Header from "@/Components/Sections/Header.vue";
+import Banner from "@/Components/Sections/Banner.vue";
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    }
+});
+
+</script>
+
+<template>
+    <Head title="Homepage" />
+    <Header :canLogin="canLogin" :canRegister="canRegister" />
+    <Banner />
+</template>
